cca6c1d198d17b53a9c1b539afba57f7851b9f2e
Deniz <s0571123@htw-berlin.de>
2022-10-24 23:17:24 +0200

    Hello World push

---
+++ src/main/java/de/htwberlin/visitenkarten/VisitenkartenApplication.java
@@ -0,0 +1,29 @@
+package de.htwberlin.visitenkarten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@SpringBootApplication
+public class VisitenkartenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisitenkartenApplication.class, args);
+	}
+
+
+
+	@Controller
+	public class GreetingController {
+
+		@GetMapping("/greeting")
+		public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+			model.addAttribute("name", name);
+			return "greeting";
+		}
+
+	}
+}
---
+++ src/test/java/de/htwberlin/visitenkarten/VisitenkartenApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwberlin.visitenkarten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitenkartenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
